b4e9577c38be9e041ffe799eb6c32c21921e8de7
Leandro Alves <leandrofelipecunha@hotmail.com>
2024-08-03 18:04:14 -0300

    feat: finish favorites implementation

--- app/api/v1/playlists.py
+++ app/api/v1/playlists.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, Depends
+from typing import Annotated, List
 
 from app.schemas.favorite import FavoriteInput, FavoriteOutput
 from app.schemas.location import Location
 from app.schemas.playlists import Playlists
 from app.services.playlist_service import PlaylistService
+from app.utils.auth_utils import AuthUtils
 
 router = APIRouter(prefix='/playlists', tags=['playlists'])
 
@@ -14,7 +16,22 @@ def get_playlists(location: Location = Depends()):
     return playlist_service.get_playlist_by_location(location)
 
 
-@router.post('/favorite', response_model=FavoriteOutput)
-def create_favorite(favorite: FavoriteInput):
+@router.get('/favorites', response_model=List[FavoriteOutput])
+def list_favorites(email: Annotated[str, Depends(
+                  AuthUtils.get_user_email_from_token)]):
     playlist_service = PlaylistService()
-    return playlist_service.create_favorite(favorite)
+    return [f.to_schema() for f in playlist_service.list_favorites(email)]
+
+
+@router.post('/favorites', response_model=FavoriteOutput)
+def create_favorite(favorite: FavoriteInput, email: Annotated[str, Depends(
+                    AuthUtils.get_user_email_from_token)]):
+    playlist_service = PlaylistService()
+    return playlist_service.create_favorite(email, favorite).to_schema()
+
+
+@router.delete('/favorites/{favorite_id}')
+def create_favorite(favorite_id: str, email: Annotated[str, Depends(
+                    AuthUtils.get_user_email_from_token)]):
+    playlist_service = PlaylistService()
+    return playlist_service.delete_favorite(email, favorite_id)
