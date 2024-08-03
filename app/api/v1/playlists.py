7887365364e18428256c40521c7823a80bb8278e
Leandro Alves <leandrofelipecunha@hotmail.com>
2024-08-03 14:40:00 -0300

    feat: include favorite output

--- app/api/v1/playlists.py
+++ app/api/v1/playlists.py
@@ -1,19 +1,20 @@
 from fastapi import APIRouter, Depends
 
-from app.schemas.favorite import FavoriteInput
+from app.schemas.favorite import FavoriteInput, FavoriteOutput
 from app.schemas.location import Location
+from app.schemas.playlists import Playlists
 from app.services.playlist_service import PlaylistService
 
 router = APIRouter(prefix='/playlists', tags=['playlists'])
 
 
-@router.get('/')
+@router.get('/', response_model=Playlists)
 def get_playlists(location: Location = Depends()):
     playlist_service = PlaylistService()
     return playlist_service.get_playlist_by_location(location)
 
 
-@router.post('/favorite')
+@router.post('/favorite', response_model=FavoriteOutput)
 def create_favorite(favorite: FavoriteInput):
     playlist_service = PlaylistService()
     return playlist_service.create_favorite(favorite)
--- app/models/favorite.py
+++ app/models/favorite.py
@@ -1,10 +1,17 @@
 from firedantic import Model
 
+from app.schemas.favorite import FavoriteOutput
 from app.schemas.weather import Weather
 from app.schemas.playlists import Playlist
 
 
 class Favorite(Model):
     __collection__ = "favorites"
+    user_id: str
     weather: Weather
     playlist: Playlist
+
+    def to_schema(self):
+        return FavoriteOutput(_id=self.id, user_id=self.user_id,
+                              weather=self.weather, playlist=self.playlist)
+        
