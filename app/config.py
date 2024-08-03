51fc73a1b87e041def8cc690a3699defd3924950
Leandro Alves <leandrofelipecunha@hotmail.com>
2024-08-03 01:57:57 -0300

    feat: include extra config vars

--- app/config.py
+++ app/config.py
@@ -1,3 +1,4 @@
+from functools import lru_cache
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -6,5 +7,14 @@ class Settings(BaseSettings):
     developer_email: str = ''
     open_weather_api_key: str = ''
     open_weather_api_base_url: str = ''
+    spotify_client_id: str = ''
+    spotify_client_secret: str = ''
+    spotify_account_base_url: str = ''
+    spotify_api_base_url: str = ''
 
     model_config = SettingsConfigDict(env_file=".env")
+
+
+@lru_cache
+def get_settings():
+    return Settings()
